13ec16c1997313e6a7ca3160c91eb6a120450327
Ilya Chursin <kapone14@yandex.ru>
2021-11-29 21:28:51 +0300

    commit

---
+++ one_plus_two.py
@@ -0,0 +1,35 @@
+import math
+import time
+import functools
+
+
+def timefunc(func):
+    @functools.wraps(func)
+    def need_time(*args, **kwargs):
+        start = time.perf_counter()
+        res = func(*args, **kwargs)
+        time_elapsed = time.perf_counter() - start
+        print(f"Функция: {func.__name__}, Затраченное время: {time_elapsed}\n")
+        return res
+
+    return need_time
+
+@timefunc
+def square():
+    SQFT_PER_ACRE = 43560
+    width = float(input("Введите ширину участка в футах\n"))
+    length = float(input("Введите длину участка в футах\n"))
+    result = (width * length) / SQFT_PER_ACRE
+    print("Площадь в акрах ->", result)
+
+@timefunc
+def speed():
+    acceleration = 9.8
+    hight = float(input("Введите высоту в метрах\n"))
+    result = math.sqrt(2*acceleration*hight)
+    print("Скорость при касании земли ->", round(result))
+
+
+
+speed()
+square()
